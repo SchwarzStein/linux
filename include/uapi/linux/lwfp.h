695bf79a5200ea08d228a82e6307490b72589590
Pamenas Kariuki <mpamenas@gmail.com>
2026-09-23 01:11:39 +0200

    fix incorrect flag

--- include/uapi/linux/lwfp.h
+++ include/uapi/linux/lwfp.h
@@ -8,7 +8,7 @@
 #define PERF_LWFP_MATCH_FLAGS     BIT(3)
 #define PERF_LWFP_MATCH_ENCLAVE   BIT(4)
 
-#define LWFP_X86_64_REG_MAX 18
+#define LWFP_X86_64_REG_MAX 16
 #define LWFP_ARM64_REG_MAX  32
 
 #if defined(__x86_64__)
